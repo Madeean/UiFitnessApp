6c7d588479aa33409a5d1c94a8b7b233d61a578d
MadeReihan <reihanmade@gmail.com>
2023-01-26 14:30:15 +0700

    register page

--- app/src/main/java/com/madeean/fitnessapp1/RegisterPage.kt
+++ app/src/main/java/com/madeean/fitnessapp1/RegisterPage.kt
@@ -2,10 +2,17 @@ package com.madeean.fitnessapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterPage : AppCompatActivity() {
+    private lateinit var btn_login:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_page)
+
+        btn_login = findViewById(R.id.tv_login)
+
+
+
     }
 }
